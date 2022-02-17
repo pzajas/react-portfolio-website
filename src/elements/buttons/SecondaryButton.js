7381be6673a2ece1e40119ce55e94921350ff253
Zajas Piotr <zajas.piotr@gmail.com>
2022-02-17 23:00:41 +0100

    Change the to-the-top-button style.

--- src/elements/buttons/SecondaryButton.js
+++ src/elements/buttons/SecondaryButton.js
@@ -1,13 +1,24 @@
 import styled from "styled-components"
-import { ChevronsUp } from "@styled-icons/boxicons-regular/ChevronsUp"
+import { ChevronUp } from "@styled-icons/boxicons-regular/ChevronUp"
 import * as style from "../../variables/Variables"
 
-const StyledButtonContainer = styled(ChevronsUp)`
+const StyledButtonContainer = styled(ChevronUp)`
   background-color: ${style.SecondaryColor};
   padding: 0.2rem 0rem;
-  width: 2.5rem;
+  width: 1.5rem;
   border-radius: 0.1rem;
   cursor: pointer;
+  animation: buttonMove 2s infinite alternate;
+
+  @keyframes buttonMove {
+    0% {
+      transform: translateY(0.1rem);
+    }
+
+    100% {
+      transform: translateY(-0.1rem);
+    }
+  }
 `
 
 const SecondaryButton = ({ className, secondaryButtonClick }) => {
